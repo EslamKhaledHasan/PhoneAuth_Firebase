091fb0d747c97c0a66df155fb7062543fbdea1b1
eslaamm66 <you@example.com>
2022-12-21 19:24:47 +0200

    register page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:phoneauth/screans/welcome_screan.dart';
+import 'package:phoneauth/screans/welcome_screen.dart';
 void main()  {
  
   runApp(const MyApp());
